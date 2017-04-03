716015159f85be577dfe33ccb898684cd3bf8ca8
Jian Chen <astercd@gmail.com>
2017-04-03 03:27:55 -0700

    Initial check-in

---
+++ BSF/ClassViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ClassViewController.swift
+//  BSF
+//
+//  Copyright © 2017 BSF CN. All rights reserved.
+//
+
+import UIKit
+
+class ClassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BSF/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  BSF
+//
+//  Copyright © 2017 BSF CN. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
